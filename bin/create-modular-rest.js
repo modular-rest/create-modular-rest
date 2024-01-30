56a3f025558c4083f0db7af25bd805203e82425a
Navid Shad <navidshad72@gmail.com>
2024-01-31 00:38:48 +0300

    Update dependencies and fix code formatting

--- bin/create-modular-rest.js
+++ bin/create-modular-rest.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 // The first argument will be the project name.
 const projectName = process.argv[2];
@@ -8,40 +8,42 @@ const projectName = process.argv[2];
 const currentDir = process.cwd();
 const projectDir = path.resolve(currentDir, projectName);
 fs.mkdirSync(projectDir, {
-	recursive: true
+  recursive: true,
 });
 
 // Copy the template directory to the project directory.
-const templateDir = path.resolve(__dirname, '..');
-fs.cpSync(templateDir, projectDir, {
-	recursive: true
-}, (err) => {});
+const templateDir = path.resolve(__dirname, "..");
+fs.cpSync(
+  templateDir,
+  projectDir,
+  {
+    recursive: true,
+  },
+  (err) => {}
+);
 
 // Remove bin dir from project
-const binDir = path.resolve(projectDir, 'bin');
-fs.rmdirSync(binDir, {
-	recursive: true
+const binDir = path.resolve(projectDir, "bin");
+fs.rmSync(binDir, {
+  recursive: true,
 });
 
 // Rename files
-const renameFiles = ['env'];
+const renameFiles = ["env"];
 renameFiles.forEach((file) => {
-	fs.renameSync(
-		path.join(projectDir, file),
-		path.join(projectDir, '.' + file)
-	);
-})
+  fs.renameSync(path.join(projectDir, file), path.join(projectDir, "." + file));
+});
 
-const projectPackageJson = require(path.join(projectDir, 'package.json'));
+const projectPackageJson = require(path.join(projectDir, "package.json"));
 
 // Update the project's package.json with the new project name
 projectPackageJson.name = projectName;
 delete projectPackageJson.bin;
 
 fs.writeFileSync(
-	path.join(projectDir, 'package.json'),
-	JSON.stringify(projectPackageJson, null, 2)
+  path.join(projectDir, "package.json"),
+  JSON.stringify(projectPackageJson, null, 2)
 );
 
-console.log('Success! Your new project is ready.');
+console.log("Success! Your new project is ready.");
 console.log(`Created ${projectName} at ${projectDir}`);
